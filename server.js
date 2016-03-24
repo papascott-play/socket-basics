d03357ee155163c2ceef71b4f89c2785b7f6a777
papascott <papascott@gmail.com>
2016-03-24 17:03:04 +0100

    Added listener to front end

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function () {
-  console.log('User connected via socket.io!');
+    console.log('User connected via socket.io!');
 });
 
 http.listen(PORT, function () {
-  console.log('Server started!');
+    console.log('Server started!');
 });
